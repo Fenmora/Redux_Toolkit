d9b6533a80c1016e56d8430ff31a2aba534add6f
fmora <fmora@cmnhospitals.org>
2022-06-24 13:57:16 -0600

    simple redux toolkit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="?">
+          React Redux App
+        </a>
+      </div>
+    </nav>
+  );
+};
